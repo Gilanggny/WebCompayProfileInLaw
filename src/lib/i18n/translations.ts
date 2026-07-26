744cf15e0b5138b2fa3e3d1ae3258a04d7f7083c
Gilanggny <52845853+Gilanggny@users.noreply.github.com>
2026-07-26 17:15:41 +0700

    update fixing teamNames

--- src/lib/i18n/translations.ts
+++ src/lib/i18n/translations.ts
@@ -91,9 +91,9 @@ export type Translations = {
 };
 
 const teamNames = [
-  "Dr. Aida Ardini S.H.,M.H.,CLA.,CCD.,CMED.",
-  "Dr. Aida Ardini S.H.,M.H.,CLA.,CCD.,CMED.",
-  "Dr. Aida Ardini S.H.,M.H.,CLA.,CCD.,CMED.",
+  // "Dr. Aida Ardini S.H.,M.H.,CLA.,CCD.,CMED.",
+  // "Dr. Aida Ardini S.H.,M.H.,CLA.,CCD.,CMED.",
+  // "Dr. Aida Ardini S.H.,M.H.,CLA.,CCD.,CMED.",
 ] as const;
 
 export const teamInitials = ["AR", "DW", "BP", "SK"] as const;
